2a164c4480e15a3f6205dc6e26ba691837196cc3
Karleigh Moore <karleighmoore19@gmail.com>
2017-04-11 15:14:10 -0400

    Functional, not pretty

--- public/javascript/addAllergies.js
+++ public/javascript/addAllergies.js
@@ -9,20 +9,31 @@
 
 
 $(document).ready(function(){
-    var allergiesList = ["eggs"];
-    for(allergy of allergiesList){
-        $("#myAllergies").append("<p>"+allergy+"</p>");
-    }
+    var allergiesList = ["eggs","milk"];
     
-    $("#submitButton").click(function(){
-        var newAllergy = $("#newAllergy").val();
-        allergiesList.push(newAllergy);
+    //TODO: need to make sure can't add duplicates
+    
+    var updateAllergies = function(){
         $("#myAllergies").empty();//so we don't add repeats
+        //go through and update the allergy list
         for(allergy of allergiesList){
-            console.log(allergy);
-            $("#myAllergies").append("<p>"+allergy+"</p>");
+            $("#myAllergies").append('<div class="allergyRow" id="'+allergy+'"Row"><button class="deleteButton" id="'+allergy+'"'+'type="button">Delete</button><p class="allergyName">'+allergy+'</p></div>');
         }
-        return false;//so that page doesn't refresh and clear the page
+    };
+
+    //http://stackoverflow.com/questions/1359018/in-jquery-how-to-attach-events-to-dynamic-html-elements
+    $("body").on("click", ".deleteButton",function(){
+        //remove that allergy from the list
+        allergiesList.splice(allergiesList.indexOf(this.id),1); //https://bytearcher.com/articles/how-to-delete-value-from-array/
+        updateAllergies();
+    });
+
+    $('body').on("click", "#submitButton", function(){
+        var newAllergy = $("#newAllergy").val();
+        $("#newAllergy").val("");
+        //add new allergy
+        allergiesList.push(newAllergy);
+        updateAllergies();
     });
 
 
