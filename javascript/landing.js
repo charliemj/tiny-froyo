ef4f27ad18510da14796df0660bf730942966111
Megan Prakash <meganp@mit.edu>
2017-04-19 13:56:20 -0400

    renamed pages so that landing page is index

--- javascript/landing.js
+++ javascript/landing.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     //Add allergies by pressing enter button
     $(document).on("keydown", function(e){ //http://stackoverflow.com/questions/16011312/execute-function-on-enter-key
         if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
-            window.location.href = "index.html";
+            window.location.href = "edit.html";
         }
     });
 });
